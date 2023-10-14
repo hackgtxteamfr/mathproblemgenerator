8ee5cc9ad30bc275a66091a82197b5a72fccb9ca
zchakraborty <113480497+zinichakraborty@users.noreply.github.com>
2023-10-14 13:55:38 -0400

    idk

---
+++ mathgen/src/app/api/health.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+    if (req.method == 'GET') {
+        try {
+            return res.status(200).json({"healthy": true})
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({"healthy": false})
+        }
+    }
+    return res.status(400).send("Incorrect req method type")
+}
