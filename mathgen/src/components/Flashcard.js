6630fdcedb1ac21b0a188868d63839a740ae2759
Nathan Papa <nathanpapa35@gmail.com>
2023-10-14 19:19:57 -0400

    Add random questions

--- mathgen/src/components/Flashcard.js
+++ mathgen/src/components/Flashcard.js
@@ -1,12 +1,43 @@
-import React from 'react';
-//import './flashcard.css';
+import { useState, useEffect } from 'react';
 
 export default function Flashcard() {
+    const operators = ["+", "-", "*", "/", "%"];
+    const [question, setQuestion] = useState({first: 0, second: 0, operator: operators[0]});
+    const [answer, setAnswer] = useState(0);
+
+    function changeQuestion() {
+        setQuestion({first: Math.floor(Math.random() * 1000), second: Math.floor(Math.random() * 1000), operator: operators[Math.floor(Math.random() * 5)], answer: 0});
+    }
+
+    function solve() {
+        if (question.operator === "+") {
+            setAnswer(question.first + question.second);
+        } else if (question.operator === "-") {
+            setAnswer(question.first - question.second);
+        } else if (question.operator === '*') {
+            setAnswer(question.first * question.second);
+        } else if (question.operator === "/") {
+            setAnswer(question.first / question.second);
+        } else if (question.operator === '%') {
+            setAnswer(question.first % question.second);
+        } else {
+            console.log("ERROR");
+        }
+    }
+
+    useEffect(() => {
+        changeQuestion();
+    }, []);
+
+    useEffect(() => {
+        solve();
+    }, [question]);
+
     return (
         <div className="flashcard">
-            <h1>2 + 2 =</h1>
+            <h1>{question.first} {question.operator} {question.second}</h1>
             <div>
-                <button>Answer 1</button>
+                <button>{answer}</button>
                 <button>Answer 2</button>
                 <button>Answer 3</button>
                 <button>Answer 4</button>
--- mathgen/src/components/Home.js
+++ mathgen/src/components/Home.js
@@ -3,7 +3,6 @@ import Fact from "./Fact";
 import Flashcard from "./Flashcard";
 import Navbar from './Navbar';
 import ContentView from './ContentView';
-//import "./Home.css"
 
 export default function Home() {
     return (
