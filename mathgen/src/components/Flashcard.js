cb8660c5e400edc3d34ad5e1997b5dff455ef78a
Nathan Papa <nathanpapa35@gmail.com>
2023-10-13 23:28:40 -0400

    Add flashcard but it looks bad

--- mathgen/src/components/Flashcard.js
+++ mathgen/src/components/Flashcard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import '../flashcard.css';
 
 export default function Flashcard() {
     return (
-        <div>
-            
+        <div className="flashcard">
+            <h1>2 + 2 =</h1>
+            <div>
+                <button>Answer 1</button>
+                <button>Answer 2</button>
+                <button>Answer 3</button>
+                <button>Answer 4</button>
+            </div>
         </div>
     );
 }
