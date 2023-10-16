b2a1c585968d35b13f89ab569a84ae79083a44f5
zchakraborty <113480497+zinichakraborty@users.noreply.github.com>
2023-10-16 14:03:16 -0400

    Update Navbar.js

--- mathgen/src/components/Navbar.js
+++ mathgen/src/components/Navbar.js
@@ -4,30 +4,59 @@ import Fact from './Fact';
 import Link from 'next/link'
 import useSWR from 'swr'
 import axios from 'axios';
+import { useState } from 'react';
+import auth from '../../server/utils/auth';
 
 
 export default function Navbar() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    
+    try {
+        auth(req)
+    } catch (e) {
+        return res.status(400).send("Not logged in as an admin")
+    }
+
     const fetcher = async (url) => {
         const res = await axios.get(url)
         return res.data
     }
 
-    let { data, isLoading, isValidating } = useSWR(`./api/points`, fetcher)
-    if (isLoading) return <div><h2>Loading</h2></div>
+    if(loggedIn) {
+        let { data, isLoading, isValidating } = useSWR(`./api/points`, fetcher)
+        if (isLoading) return <div><h2>Loading</h2></div>
 
-    return (
-        <div className="navbar">
-            <div className="login">
-                <p>&nbsp;</p>
+        return (
+            <div className="navbar">
+                <div className="login">
+                    <p>&nbsp;</p>
+                </div>
+                <h1>Math Problem Generator</h1>
+                <h2>{data}</h2>
+                <div className="login">
+                <Link href="./login">Login</Link>
+                    <a href="#">
+                        <img src="./profile.png" className="icon"/>
+                    </a>
+                </div>
             </div>
-            <h1>Math Problem Generator</h1>
-            <h2>{data}</h2>
-            <div className="login">
-            <Link href="./login">Login</Link>
-                <a href="#">
-                    <img src="./profile.png" className="icon"/>
-                </a>
+        );
+    }
+
+    if (!loggedIn) {
+        return (
+            <div className="navbar">
+                <div className="login">
+                    <p>&nbsp;</p>
+                </div>
+                <h1>Math Problem Generator</h1>
+                <div className="login">
+                <Link href="./login">Login</Link>
+                    <a href="#">
+                        <img src="./profile.png" className="icon"/>
+                    </a>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
